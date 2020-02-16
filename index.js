87cc0698e8511d2c06e05a5f95445f1218d4a675
Chris5407 <christopher.smith4202@gmail.com>
2020-02-16 16:12:17 -0600

    Setup basic node.js server

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const exphbs  = require('express-handlebars');
+
+const app = express();
+const port = 3000;
+
+var hbs = exphbs.create({
+	extname: '.html'
+});
+
+app.engine('html', hbs.engine);
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+	res.render("home", {value: "Something"});
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
